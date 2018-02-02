d3fd6516f0fac8587ddd618517b29cdfc9dde3e7
sundarcodes <sundar82@gmail.com>
2018-02-02 21:31:23 +0530

    Minimal working version

--- src/state.elm
+++ src/state.elm
@@ -3,13 +3,8 @@ module State exposing (..)
 import Array exposing (..)
 
 
-type MarkerStatus
-    = NotMarked
-    | Color
-
-
 type alias Box =
-    { mark : MarkerStatus
+    { mark : Color
     , pos : ( Int, Int )
     }
 
@@ -17,11 +12,12 @@ type alias Box =
 type Color
     = Green
     | Orange
+    | White
 
 
 type Msg
     = Undo
-    | Mark Color
+    | Mark ( Int, Int )
     | Reset
 
 
@@ -38,7 +34,7 @@ initialModel =
                     Array.fromList
                         ((List.map
                             (\col ->
-                                { mark = NotMarked
+                                { mark = White
                                 , pos = ( row, col )
                                 }
                             )
@@ -53,6 +49,107 @@ initialModel =
     }
 
 
+markBoxWithColor : Color -> Array (Array Box) -> ( Int, Int ) -> Array (Array Box)
+markBoxWithColor color boxes ( x, y ) =
+    let
+        row =
+            case Array.get x boxes of
+                Nothing ->
+                    fromList []
+
+                Just val ->
+                    val
+
+        box =
+            case Array.get y row of
+                Nothing ->
+                    { mark = White
+                    , pos = ( x, y )
+                    }
+
+                Just val ->
+                    val
+    in
+        Array.set x (Array.set y { mark = color, pos = ( x, y ) } row) boxes
+
+
+deduceNextMove : Color -> Color
+deduceNextMove color =
+    if (color == Orange) then
+        Green
+    else
+        Orange
+
+
+isGameOver : Array (Array Box) -> Bool
+isGameOver arr =
+    -- First check the rows
+    -- Check the coloums
+    -- Check the left diagonal
+    -- Check the right diagonal
+    False
+
+
+isAllBoxSameColor : Array Box -> Bool
+isAllBoxSameColor arr =
+    let
+        totalLength =
+            Array.length arr
+
+        numOfOranges =
+            Array.length (Array.filter (\box -> box.mark == Orange) arr)
+
+        numOfGreen =
+            Array.length (Array.filter (\box -> box.mark == Green) arr)
+    in
+        if (totalLength == numOfOranges) || (totalLength == numOfGreen) then
+            True
+        else
+            False
+
+
+isMoveValid : Array (Array Box) -> ( Int, Int ) -> Bool
+isMoveValid boxes ( x, y ) =
+    let
+        row =
+            case Array.get x boxes of
+                Nothing ->
+                    fromList []
+
+                Just val ->
+                    val
+
+        box =
+            case Array.get y row of
+                Nothing ->
+                    { mark = White
+                    , pos = ( x, y )
+                    }
+
+                Just val ->
+                    val
+    in
+        if box.mark /= White then
+            False
+        else
+            True
+
+
 update : Msg -> Model -> Model
 update msg model =
-    model
+    case msg of
+        Mark ( x, y ) ->
+            if isMoveValid model.boxes ( x, y ) then
+                { model
+                    | nextMove = deduceNextMove model.nextMove
+                    , boxes = markBoxWithColor model.nextMove model.boxes ( x, y )
+                    , isGameOver = isGameOver model.boxes
+                }
+            else
+                model
+
+        Undo ->
+            model
+
+        Reset ->
+            model
--- src/view.elm
+++ src/view.elm
@@ -7,24 +7,33 @@ import Array exposing (..)
 import Html.Events exposing (onClick)
 
 
-row : Array Box -> Color -> Html Msg
-row arr nextMove =
-    div [ class "row" ] (List.map (\box -> placeHolder box nextMove) (Array.toList arr))
+row : Array Box -> Html Msg
+row arr =
+    div [ class "row" ] (List.map (\box -> placeHolder box) (Array.toList arr))
 
 
-placeHolder : Box -> Color -> Html Msg
-placeHolder box nextMove =
-    div
-        [ class "col-md-4 place-holder"
-        , id (toString (Tuple.first box.pos) ++ "-" ++ toString (Tuple.second box.pos))
-        , onClick (State.Mark nextMove)
-        ]
-        []
+placeHolder : Box -> Html Msg
+placeHolder box =
+    let
+        className =
+            if box.mark == State.Orange then
+                "orange"
+            else if box.mark == State.Green then
+                "green"
+            else
+                ""
+    in
+        div
+            [ class ("col-md-4 place-holder" ++ " " ++ className)
+            , id (toString (Tuple.first box.pos) ++ "-" ++ toString (Tuple.second box.pos))
+            , onClick (State.Mark box.pos)
+            ]
+            []
 
 
 view : Model -> Html Msg
 view model =
     div [ class "container" ]
         [ div [ id "grid" ]
-            (Array.toList (Array.map (\rowElement -> row rowElement model.nextMove) (model.boxes)))
+            (Array.toList (Array.map (\rowElement -> (row rowElement)) (model.boxes)))
         ]
